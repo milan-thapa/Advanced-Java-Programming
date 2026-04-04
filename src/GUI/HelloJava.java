be88237f7a7f12fd940e6d6d2daf350524a0f106
Milan Thapa <thapamilan9762@gmail.com>
2026-04-04 08:57:52 +0545

    deleted unnecessary files

---
+++ src/GUI/HelloJava.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class HelloJava {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/GUI/HelloSwing.java
@@ -0,0 +1,7 @@
+package GUI;
+
+public class HelloSwing {
+    public static void main(String[] args){
+
+    }
+}
